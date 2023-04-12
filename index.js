33dca423aa8a379670abe620db98122800695549
Rk-Dinesh <127471925+Rk-Dinesh@users.noreply.github.com>
2023-04-12 11:04:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function fun() {
+    let value1 = document.getElementById("FirstName").value;
+    console.log("FirstName: " + value1);
+    let value2 = document.getElementById("MiddleName").value;
+    console.log("MiddleName: " + value2);
+    let value3 = document.getElementById("LastName").value;
+    console.log("LastName: " + value3);
+    let mail = document.getElementById("mail").value;
+    console.log("Email: " + mail);
+    let addr = document.getElementById("Address").value;
+    console.log("Address: " + addr);
+}
